d1f0be5be04e9feb43ee8f412876c05daa49769b
CJ Carey <perimosocordiae@gmail.com>
2015-11-10 12:59:56 -0500

    Fixing encoding bug

--- superman/traj/all_pairs.py
+++ superman/traj/all_pairs.py
@@ -6,7 +6,7 @@ pyx_file = tpl_file[:-3]
 if (not os.path.exists(pyx_file) or
     os.path.getmtime(tpl_file) > os.path.getmtime(pyx_file)):
   from Cython import Tempita
-  tpl = Tempita.Template.from_filename(tpl_file)
+  tpl = Tempita.Template.from_filename(tpl_file, encoding='utf-8')
   with open(pyx_file, 'w') as fh:
     fh.write(tpl.substitute())
 
