fc8dce320ee305620568520d8a93ed47c27c56aa
CJ Carey <perimosocordiae@gmail.com>
2016-03-22 15:24:28 -0500

    Starting to add non-pyximport cython setup

--- setup.py
+++ setup.py
@@ -1,8 +1,16 @@
 #!/usr/bin/env python
 from glob import glob
-from setuptools import setup, find_packages
+from setuptools import setup, find_packages, Extension
 
-setup(
+try:
+  from Cython.Build import cythonize
+  import numpy as np
+except ImportError:
+  use_cython = False
+else:
+  use_cython = True
+
+setup_kwargs = dict(
     name='superman',
     version='0.0.1',
     author='CJ Carey',
@@ -22,3 +30,15 @@ setup(
     ],
     scripts=glob('scripts/*.py'),
 )
+
+if use_cython:
+  exts = [
+      Extension('*', ['superman/*.pyx'], include_dirs=[np.get_include()],
+                extra_compile_args=['-Ofast', '-fopenmp', '-march=native',
+                                    '-Wno-unused-function'],
+                extra_link_args=['-Ofast', '-fopenmp', '-march=native']),
+  ]
+  setup_kwargs['ext_modules'] = cythonize(exts)
+
+
+setup(**setup_kwargs)
