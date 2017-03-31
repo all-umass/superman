60cf8e5b62233fb8661b743d9763f76a9c8260b1
CJ Carey <perimosocordiae@gmail.com>
2017-03-31 17:23:28 -0400

    Adding unary NOT operator to search queries

--- superman/dataset/_search.py
+++ superman/dataset/_search.py
@@ -22,17 +22,19 @@ def _parse_query(query, case_sensitive):
   if not tokens:
     return None
 
-  # AND expressions bind tightest, then OR, then adjacency (implicit AND)
-  for lexeme, expr_type in [(u'AND', AndExpr), (u'OR', OrExpr)]:
+  # binding precedence: NOT > AND > OR > adjacency (implicit AND)
+  op_precedence = [(u'NOT', NotExpr), (u'AND', AndExpr), (u'OR', OrExpr)]
+  for lexeme, expr_type in op_precedence:
     while True:
-      idxs = [i for i, x in enumerate(tokens)
-              if not isinstance(x, QueryExpr) and x.upper() == lexeme]
-      if not idxs:
+      idx = _find_keyword(lexeme, tokens)
+      if idx is None:
         break
-      idx = idxs[0]
-      lhs = tokens[idx-1]
       rhs = tokens[idx+1]
-      tokens = tokens[:idx-1] + [expr_type(lhs, rhs)] + tokens[idx+2:]
+      if expr_type.arity == 2:
+        lhs = tokens[idx-1]
+        tokens = tokens[:idx-1] + [expr_type(lhs, rhs)] + tokens[idx+2:]
+      else:  # unary expr
+        tokens = tokens[:idx] + [expr_type(rhs)] + tokens[idx+2:]
 
   # AND together any remaining roots
   while len(tokens) > 1:
@@ -43,6 +45,13 @@ def _parse_query(query, case_sensitive):
   # get a matcher function from the root
   root, = tokens
   return _matcher(root, case_sensitive)
+
+
+def _find_keyword(lexeme, tokens):
+  for i, tok in enumerate(tokens):
+    if not isinstance(tok, QueryExpr) and tok.upper() == lexeme:
+      return i
+  return None
 
 
 def _matcher(token, case_sensitive):
@@ -56,26 +65,41 @@ def _matcher(token, case_sensitive):
 
 
 class QueryExpr(object):
-  def __init__(self, lhs, rhs):
-    self.lhs = lhs
-    self.rhs = rhs
+  def __init__(self, arg1, arg2=None):
+    self.arg1 = arg1
+    self.arg2 = arg2
 
 
 class AndExpr(QueryExpr):
+  arity = 2
+
   def matcher(self, case=False):
-    lhs = _matcher(self.lhs, case)
-    rhs = _matcher(self.rhs, case)
+    lhs = _matcher(self.arg1, case)
+    rhs = _matcher(self.arg2, case)
     return lambda text: lhs(text) and rhs(text)
 
   def __repr__(self):
-    return u'(AND: %r %r)' % (self.lhs, self.rhs)
+    return u'(AND: %r %r)' % (self.arg1, self.arg2)
 
 
 class OrExpr(QueryExpr):
+  arity = 2
+
   def matcher(self, case=False):
-    lhs = _matcher(self.lhs, case)
-    rhs = _matcher(self.rhs, case)
+    lhs = _matcher(self.arg1, case)
+    rhs = _matcher(self.arg2, case)
     return lambda text: lhs(text) or rhs(text)
 
   def __repr__(self):
-    return u'(OR: %r %r)' % (self.lhs, self.rhs)
+    return u'(OR: %r %r)' % (self.arg1, self.arg2)
+
+
+class NotExpr(QueryExpr):
+  arity = 1
+
+  def matcher(self, case=False):
+    expr = _matcher(self.arg1, case)
+    return lambda text: not expr(text)
+
+  def __repr__(self):
+    return u'(NOT: %r)' % (self.arg1,)
